8bc9d7726c9f94e6ed3581921b587dc63f6fb3fa
JulSarmiento <jasa1999@hotmail.com>
2021-11-02 16:00:51 -0500

    Pagina de recibir dinero mock funcionando

--- public/scripts/pages/reception.js
+++ public/scripts/pages/reception.js
@@ -15,21 +15,22 @@ window.addEventListener('load', () => {
   document.getElementById('reception-form').addEventListener('submit', (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
+    const description = data.get('description');
     const amountRecept = parseInt(data.get('amount-to-recep'));
 
     reception(amountRecept);
 
     setTimeout(() =>{
       document.querySelector('.qr-picture').classList.toggle('hide');
-    }, 2000);
+    }, 1000);
 
 
     setTimeout(() => {
       validator.balance += amountRecept;
-      validator.movements.push({name: 'Recepcion', type: 'Recepcion', amount: amountRecept});
+      validator.movements.push({name: description, type: 'Recepcion', amount: amountRecept});
       localStorage.setItem('Users', JSON.stringify(UserFactory.users));
       alert (`Recepcion exitosa por ${formatPrice(amountRecept)}, su nuevo saldo es de: ${formatPrice(validator.balance)}`);
-    }, 6000);
+    }, 5000);
 
 
     
